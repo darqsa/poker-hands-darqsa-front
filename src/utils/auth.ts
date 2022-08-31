9abc743e235dfefc274e28e4620c1ff1961dc032
David Arques <david.darqsa@gmail.com>
2022-08-31 18:34:55 +0200

    Added login user hook

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,11 +1,12 @@
 import jwt from "jwt-decode";
 
-const fetchToken = (
-  token: string
-): {
-  id: string;
-  username: string;
-  iat: number;
-} => jwt(token);
+const fetchToken = (token: string) => {
+  const payloadToken: {
+    id: string;
+    username: string;
+    iat: number;
+  } = jwt(token);
 
+  return { token: token, id: payloadToken.id, username: payloadToken.username };
+};
 export default fetchToken;

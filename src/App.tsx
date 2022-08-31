fa9bc2debbaea2d146f248e31957501a3c563184
David Arques <david.darqsa@gmail.com>
2022-08-31 20:30:02 +0200

    Added Login Page and redirect to register Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Navigate to="/" />} />
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>

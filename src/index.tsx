822c0d7d2ae2184732aef6281ab56af70e6bb1eb
David Arques <david.darqsa@gmail.com>
2022-09-01 16:38:46 +0200

    Changed index.css into a GlobalStyles file

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,6 @@ import "@fontsource/inter/400.css";
 import "@fontsource/inter/500.css";
 import "@fontsource/inter/700.css";
 import "@fontsource/inter/900.css";
-import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
 const container = document.getElementById("root")!;

d080fdf4598516d44d8d503fe406ac13db5a0559
David Arques <david.darqsa@gmail.com>
2022-09-01 12:31:52 +0200

    Added auto login function in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+import { useAppDispatch } from "./app/hooks";
+import { loginUserActionCreator } from "./features/users/slices/userSlice";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import fetchToken from "./utils/auth";
 
 function App() {
+  const token = localStorage.getItem("token");
+  const dispatch = useAppDispatch();
+
+  if (token) {
+    const user = fetchToken(token);
+    dispatch(loginUserActionCreator(user));
+  }
+
   return (
     <>
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/home" element={<RegisterPage />} />
       </Routes>
     </>
   );
